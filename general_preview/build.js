770aeaf677dd1fe4ef192b2d45504a5b3027738b
Yanflint <he.yanichkin@yandex.ru>
2025-10-13 22:42:57 +0300

    new1

--- general_preview/build.js
+++
@@ -1,8 +0,0 @@
-// local dev fallback (CI will overwrite this on deploy)
-(function(){
-  window.__BUILD__ = window.__BUILD__ || {
-    run: String(Date.now()),
-    branch: (location.pathname.indexOf('/wip/') !== -1) ? 'wip' : 'main',
-    ts: new Date().toISOString()
-  };
-})();
